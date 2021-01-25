d88b3c578b10adfde0f856616bc5ab6df6f42eae
shoman <northstr9928@gmail.com>
2021-01-25 17:23:17 +0600

    eventlistener

---
+++ new.js
@@ -0,0 +1,33 @@
+
+
+// function evenify(num){
+
+//     if(num % 2 == 0){
+//         console.log(num, ':even number');
+//     }
+//     else if(num == 0 ){
+//         console.log("shoman")
+//     }else{
+//         console.log(num * 2, ':old number');
+//     }
+// }
+// function evenify_all(nums){
+//     for (let i = 0; i < nums.length; i++){
+//         const num = nums[i];
+//         evenify(num)
+        
+    
+//     }
+
+// }
+
+
+
+
+
+// let nums = [3, 33, 23, 2, 4, "shoman"];
+// evenify_all(nums)
+
+
+// let afg = ["shoman"];
+// evenify_all(nums)
